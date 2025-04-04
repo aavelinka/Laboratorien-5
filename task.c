319337b8865f8d44d57b3486335e74166513c217
aavelinka <avelinaklimova@gmail.com>
2025-04-04 21:04:38 +0300

    task.c

---
+++ task.c
@@ -0,0 +1,42 @@
+#include<function.h>
+
+void task1() {
+    struct Stack *begin = NULL;
+    int amount; 
+    printf("Please, enter the desired amout of elements: ");
+    if(input_Number(&amount) < 0 ){
+       puts("Your request is incorrect. try again!\n");
+       return;
+    }
+   input_Stack(&begin, amount);
+   if(begin == NULL) {
+       puts("Stack is empty");
+       return;
+   } else {
+       puts("Contents of Stack:");
+       view_Stack(begin, amount);
+   }
+   int sum = sum_Stack(begin);
+   printf("The sum of stack elements is %d.\n", sum);
+   free_Stack(begin);
+   }
+
+void task2() {
+    struct Stack *Stack_1 = NULL;
+    struct Stack *Stack_2 = NULL;
+    struct Stack *Stack_Result = NULL;
+    int amount; 
+    printf("Please, enter the desired amout of elements: ");
+    if(input_Number(&amount) < 0 ){
+       puts("Your request is incorrect. try again!\n");
+       return;
+    }
+   puts("Stack 1: ");
+   input_Stack_ascending(&Stack_1, amount);
+   puts("Stack 2: ");
+   input_Stack_ascending(&Stack_2, amount);
+   make_Stack_Resalt(Stack_1, Stack_2, Stack_Result);
+   amount *= 2;
+   view_Stack(Stack_Result, amount);
+   free_Stack(Stack_Result);
+}
